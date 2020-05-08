c215f94e71992bb65d9552fdc4000deea2063fc6
josephCurto <joey.curtog@gmail.com>
2020-05-07 23:51:50 -0400

    Add attributes and comments

--- accident.py
+++ accident.py
@@ -1,6 +1,7 @@
 from pathlib import PureWindowsPath
 import pandas as pd
 from sklearn.metrics import mean_absolute_error
+import numpy as np
 
 # From other tutorial only if I need them
 import matplotlib.pyplot as pyplot
@@ -16,9 +17,17 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
+
+'''
+5/7/2020 
+Having a difficult time increasing the accuracy. Despite using binning on the continuous/numerical values, the accuracy
+tends to stay in the upper 60's. I'll try to add the city and county in which the accidents occurred into see if that
+changes anything. 
+Next step after increasing the accuracy is to clean up the code and remove redundancy; then comes the visualizations.
+'''
 # define location of dataset and read csv
 def data_preliminaries():
-    path = PureWindowsPath('file path')
+    path = PureWindowsPath('C:\\Users\\joeyc\\Desktop\\US_Accidents_Dataset.csv')
     file = pd.read_csv(path)
     return file
 
@@ -27,7 +36,7 @@ def update_dataset():
     dataset = data_preliminaries()
 
     dataset['Distance(mi)'].fillna((dataset['Distance(mi)'].mean()), inplace=True)
-    dataset['Temperature(F)'].fillna((dataset['Temperature(F)'].mean()), inplace=True)
+    dataset['Temperature'].fillna((dataset['Temperature'].mean()), inplace=True)
     dataset['Visibility'].fillna((dataset['Visibility'].mean()), inplace=True)
 
     dataset['Precipitation(in)'].fillna(0, inplace=True)
@@ -36,12 +45,22 @@ def update_dataset():
     dataset.dropna(subset=['Weather_Condition', 'Sunrise_Sunset'], how='any', inplace=True)
     return dataset
 
+def temperature_binning():
+    data_set = update_dataset()
+    data_set['Temperature_Groups'] = pd.qcut(data_set['Temperature'], q=4)
+    return data_set
+
+def temperaure_attribute_conversion():
+    data_set = temperature_binning()
+    data_set['Temperature_Groups'] = data_set['Temperature_Groups'].cat.codes
+    updated_dataset = data_set.copy()
+    return updated_dataset
+
 # Improve binning
 # Bin the visibility attribute values to attempt to improve accuracy
 def visibility_binning():
-    data_set = update_dataset()
-    data_set['Visibility_Groups'] = pd.cut(data_set.Visibility, bins=[0.0, 4.0, 8.0, 12.0, 16.0, 100.0],
-                                           labels=['0-4', '4-8', '8-12', '12-16', '16-100'])
+    data_set = temperaure_attribute_conversion()
+    data_set['Visibility_Groups'] = pd.cut(data_set.Visibility, bins=np.linspace(0, 30, 10))
     return data_set
 
 # Newly binned visibility values turned into numerical values to be read by classification machine learning algorithms
@@ -94,13 +113,19 @@ def nominal_conversion(attribute, attribute_type):
 One-Hot encoding is used for converting the string value of Sunrise_Sunset, Crossing, Junction and Traffic_Signal into
 a numerical value used for the classification machine learning algorithm
 '''
-def sun_categorical_conversion():
+def side_categorical_conversion():
     data_set = weather_categorical_conversion()
+    updated_dataset = data_set.copy()
+    updated_dataset = pd.get_dummies(updated_dataset, columns=['Side'], prefix=['Side'])
+    return updated_dataset
+
+def sun_categorical_conversion():
+    data_set = side_categorical_conversion()
     updated_dataset = data_set.copy()
     updated_dataset = pd.get_dummies(updated_dataset, columns=['Sunrise_Sunset'], prefix=['Sun'])
     return updated_dataset
 
-
+# The following methods reduce the accuracy of the machine learning algorithms.
 def crossing_categorical_conversion():
     data_set = sun_categorical_conversion()
     updated_dataset = data_set.copy()
@@ -128,25 +153,27 @@ def model():
     y = accident_data.Severity
     accident_features = ['Distance(mi)',
                          'State',
-                         'Temperature(F)',
-                         'Wind_Speed(mph)',
+                         'Temperature_Groups',
                          'Precipitation(in)',
+                         'Wind_Speed(mph)',
                          'Weather_Condition',
-                         'Sun_Night',
-                         'Sun_Day',
-                         'Visibility_Groups',
+                         'Side_L',
+                         'Side_R',
                          'Crossing_False',
                          'Crossing_True',
                          'Junction_False',
                          'Junction_True',
                          'Signal_False',
-                         'Signal_True']
+                         'Signal_True',
+                         'Sun_Night',
+                         'Sun_Day',
+                         'Visibility_Groups']
     X = accident_data[accident_features]
 
     print(X.describe())
 
     train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1, test_size=0.2, shuffle=False)
-    accident_model = GaussianNB()
+    accident_model = DecisionTreeClassifier()
     accident_model.fit(train_X, train_y)
 
     severity_predictions = accident_model.predict(val_X)
@@ -159,12 +186,9 @@ def model():
     print(confusion_matrix(val_y, severity_predictions))
     print(classification_report(val_y, severity_predictions))
 
-
     models = []
     models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
     models.append(('LDA', LinearDiscriminantAnalysis()))
-    models.append(('CART', DecisionTreeClassifier()))
-
     # evaluate each model in turn
     results = []
     names = []
@@ -183,16 +207,20 @@ def model():
 # Calls all necessary methods
 data_preliminaries()
 update_dataset()
+temperature_binning()
+temperaure_attribute_conversion()
 visibility_binning()
 visibility_categorical_conversion()
 state_categorical_conversion()
 weather_categorical_conversion()
+side_categorical_conversion()
 sun_categorical_conversion()
 crossing_categorical_conversion()
 junction_categorical_conversion()
 trafficsignal_categorical_conversion()
 model()
 '''
+
 Add back to training data if need be, however these lowered the accuracy.                   
                         'Crossing_False',
                         'Crossing_True',
